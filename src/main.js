186aa5f746d60f38676e0c54c8847a3bf548b76a
liuboringbook <1287128847@qq.com>
2020-02-23 11:34:52 +0800

    登录

--- src/main.js
+++ src/main.js
@@ -3,12 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import {Button,Input,Form,FormItem} from 'element-ui'
+import {Button,Input,Form,FormItem} from 'element-ui';
+import {Message} from 'element-ui'
 //的导入字体图标
 import './assets/fonts/iconfont.css'
 //引入全局样式
 import  './assets/css/global.css'
 
+import axios from 'axios'
+
+//配置请求的根路径
+Vue.prototype.$http =axios;
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/';
+
+//导入element-ui中的message组件
+Vue.prototype.$message =Message;
 
 Vue.use(Button);
 Vue.use(Input);
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,37 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/login'
+import Home from '@/components/home'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
+    {
+      path:'/',
+      redirect:'/login'
+    },
+    {
+      path:'/home',
+      component:Home
+    },
     {
       path: '/login',
       name: 'login',
       component: Login
     }
   ]
+});
+
+//挂载路由导航守卫
+router.beforeEach((to,from,next)=>{
+  //to 将要访问的路径
+  //form 代表从哪个路径跳转而来
+  //next 是一个函数，表示放行
+  if(to.path =='/login') return next();
+  //获取token
+ const tokenStr = window.sessionStorage.getItem('token');
+ if(!tokenStr) return next('/login');
+  next();
 })
+export default router
